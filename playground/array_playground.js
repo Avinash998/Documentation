10e4e03e04c998f6e7de028fe575a15e5dc4b2c7
Avinash Chourasia <avinashgolu.chourasia@gmail.com>
2025-06-10 10:44:18 +0530

    Playground added

---
+++ playground/array_playground.js
@@ -0,0 +1,39 @@
+// Array program 1: 
+//WAP to find the count of minimum occerance of common element of both the arrays.
+//A = [ '1', '2', '3', '2', '1', '3', '4', '4', '4', '5']
+//B = [ '2', '3', '4', '5', '5', '7', '4']
+//Output = { "2": 1, "3": 1, "4": 2, "5":1}
+
+let a = [ '1', '2', '3', '2', '1', '3', '4', '4', '4', '5'];
+let b = [ '2', '3', '4', '5', '5', '7', '4'];
+let opSet = new Set(a.concat(b));
+let opSeta = new Set(a);
+let opSetb = new Set(b);
+let opMapa = {};
+let opMapb = {};
+let opMap = {};
+for(let key of opSet){
+  if(!(opSeta.has(key)&&opSetb.has(key))){
+    opSet.delete(key)
+  }
+  else{
+    opMap[key] = 0;
+    opMapa[key] = 0;
+    opMapb[key] = 0;
+  }
+}
+for(let i=0; i<a.length; i++){
+  opMapa[a[i]]++;
+}
+for(let i=0; i<b.length; i++){
+  opMapb[b[i]]++;
+}
+for(let key in opMap){
+  if(opMapa[key]<opMapb[key]){
+    opMap[key]= opMapa[key]
+  }
+  else{
+    opMap[key] = opMapb[key]
+  }
+}
+console.log(opMap);
